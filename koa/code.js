e6db27425725ae668864ebd6c83287d1bb0e6bc1
Rotemy <RotemYo@icloud.com>
2019-04-17 14:53:12 +0300

    Update code.js

--- koa/code.js
+++ koa/code.js
@@ -1,5 +1,27 @@
 
+// simple REST call 
 
+```javascript
+const Koa = require('koa')
+const app = new Koa()
+
+const myAsync = () => new Promise(resolve => setTimeout(resolve, 2000, 'Hello worldsss'))
+
+
+app.use(async (ctx) => {
+    const body = []
+    body.push(await myAsync())
+    //body.push(await myAsync())
+    ctx.body = {
+      status: 'active',
+      message: 'This is the activation body text'
+    }
+    ctx.status = 201
+    return ctx
+})
+
+app.listen(3000)
+```
 
 // Return response
 ctx.throw(401, 'access_denied', { user: user });
