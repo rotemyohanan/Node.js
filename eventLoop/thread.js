e626cea143d5de8945e8c6c749b814b05cab5a91
rotem <rotemy.yo@gmail.com>
2020-10-02 14:05:44 +0300

    core(eventloop): thread pool size changes

--- eventLoop/thread.js
+++ eventLoop/thread.js
@@ -1,11 +1,49 @@
-const crypto = require('crypto')
+// process.env.UV_THREADPOOL_SIZE = 10 // define the number of threads in the threadpool (libuv)
 
+const crypto = require('crypto')
+// console.log(`Threads: ${process.env.UV_THREADPOOL_SIZE}`)
 const start = Date.now()
 // gives an asynchronous Password-Based Key Derivation Function 2
 // crypto.pbkdf2( password, salt, iterations, keylen, digest, callback )
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
     const end = Date.now()
     const diffTime = end-start
-    console.log('the process time: ' + diffTime)
+    console.log('(1) process time: ' + diffTime)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    const end = Date.now()
+    const diffTime = end-start
+    console.log('(2) process time: ' + diffTime)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    const end = Date.now()
+    const diffTime = end-start
+    console.log('(3) process time: ' + diffTime)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    const end = Date.now()
+    const diffTime = end-start
+    console.log('(4) process time: ' + diffTime)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    const end = Date.now()
+    const diffTime = end-start
+    console.log('(5) process time: ' + diffTime)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    const end = Date.now()
+    const diffTime = end-start
+    console.log('(6) process time: ' + diffTime)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    const end = Date.now()
+    const diffTime = end-start
+    console.log('(7) process time: ' + diffTime)
 })
 
